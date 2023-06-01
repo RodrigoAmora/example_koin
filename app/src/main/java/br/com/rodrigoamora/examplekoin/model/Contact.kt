e27f87c592f71e3c49069496f0cb76e49971af56
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-01 02:41:06 -0300

    Implementando a interface Serializable

--- app/src/main/java/br/com/rodrigoamora/examplekoin/model/Contact.kt
+++ app/src/main/java/br/com/rodrigoamora/examplekoin/model/Contact.kt
@@ -3,19 +3,13 @@ package br.com.rodrigoamora.examplekoin.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity
-class Contact {
-
+class Contact (
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0
-
-    @ColumnInfo(name = "name")
-    val name: String = ""
-
-    @ColumnInfo(name = "cellphone")
-    val cellphone: String = ""
-
-    @ColumnInfo(name = "email")
-    val email: String = ""
-}
+    val id: Long = 0L,
+    var name: String,
+    var cellphone: String ,
+    var email: String = ""
+): Serializable
