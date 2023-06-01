cdc0e4f81f0b30380928880dfad28de932dc4b22
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-01 06:33:27 -0300

    Criando os modules em variáveis

--- app/src/main/java/br/com/rodrigoamora/examplekoin/di/KoinModules.kt
+++ app/src/main/java/br/com/rodrigoamora/examplekoin/di/KoinModules.kt
@@ -6,21 +6,36 @@ import br.com.rodrigoamora.examplekoin.repository.ContactRepository
 import br.com.rodrigoamora.examplekoin.repository.impl.ContactRepositoryImpl
 import br.com.rodrigoamora.examplekoin.ui.viewmodel.ContactViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.GlobalContext.loadKoinModules
 import org.koin.dsl.module
 
-val dbModule = module {
+fun injectFeature() = loadFeature
+
+private val loadFeature by lazy {
+    loadKoinModules(
+        listOf(
+            databaseModule,
+            repositoryModule,
+            viewModelModule)
+    )
+}
+
+val databaseModule = module {
     single {
         Room.databaseBuilder(
-        get(),
-        AppDatabase::class.java,
-        "example-koin-database")
-        .build()
+            get(),
+            AppDatabase::class.java,
+            "example-koin-database")
+            .build()
     }
 
     single { get<AppDatabase>().contactDao() }
 }
 
-val appModule = module {
-    single<ContactRepository> { ContactRepositoryImpl() }
+val repositoryModule = module {
+    single<ContactRepository> { ContactRepositoryImpl(get()) }
+}
+
+val viewModelModule = module {
     viewModel { ContactViewModel(get()) }
 }
