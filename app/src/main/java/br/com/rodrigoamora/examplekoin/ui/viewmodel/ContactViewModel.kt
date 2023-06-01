477fd7355043e6f4bea1338ea1461f8381756f8e
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-01 04:15:24 -0300

    Herdando de ViewModel

--- app/src/main/java/br/com/rodrigoamora/examplekoin/ui/viewmodel/ContactViewModel.kt
+++ app/src/main/java/br/com/rodrigoamora/examplekoin/ui/viewmodel/ContactViewModel.kt
@@ -1,13 +1,15 @@
 package br.com.rodrigoamora.examplekoin.ui.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import br.com.rodrigoamora.examplekoin.model.Contact
 import br.com.rodrigoamora.examplekoin.repository.ContactRepository
 import br.com.rodrigoamora.examplekoin.repository.Resource
 
 class ContactViewModel(
     private val contactRepository: ContactRepository
-) {
+): ViewModel() {
+
 
 
     fun getContacts(): LiveData<Resource<List<Contact>?>> {
