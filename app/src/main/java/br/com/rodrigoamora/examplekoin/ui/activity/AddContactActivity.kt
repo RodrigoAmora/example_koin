5361f2bdacc2cee894e713eff8842a1f3c72cc59
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-01 02:47:09 -0300

    Criando a tela da adicionar contato

---
+++ app/src/main/java/br/com/rodrigoamora/examplekoin/ui/activity/AddContactActivity.kt
@@ -0,0 +1,14 @@
+package br.com.rodrigoamora.examplekoin.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.rodrigoamora.examplekoin.R
+
+class AddContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+    }
+
+}
