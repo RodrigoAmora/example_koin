2d39ad3e11705f8f0548edee4181fb75186aa039
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-01 06:30:45 -0300

    Salvando o contato

--- app/src/main/java/br/com/rodrigoamora/examplekoin/repository/impl/ContactRepositoryImpl.kt
+++ app/src/main/java/br/com/rodrigoamora/examplekoin/repository/impl/ContactRepositoryImpl.kt
@@ -2,19 +2,42 @@ package br.com.rodrigoamora.examplekoin.repository.impl
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import br.com.rodrigoamora.examplekoin.asynctask.BaseAsyncTask
+import br.com.rodrigoamora.examplekoin.database.dao.ContactDao
 import br.com.rodrigoamora.examplekoin.model.Contact
 import br.com.rodrigoamora.examplekoin.repository.ContactRepository
 import br.com.rodrigoamora.examplekoin.repository.Resource
+import org.koin.android.ext.android.inject
 
-class ContactRepositoryImpl: ContactRepository {
+
+class ContactRepositoryImpl(
+    private val contactDao: ContactDao
+): ContactRepository {
 
     private val mediator = MediatorLiveData<Resource<List<Contact>?>>()
 
     override fun getContacts(): LiveData<Resource<List<Contact>?>> {
-//        mediator.addSource(contactDao.findAll()) { contactsFound ->
-//            mediator.value = Resource(contactsFound)
-//        }
+        mediator.addSource(contactDao.findAll()) { contactsFound ->
+            mediator.value = Resource(contactsFound)
+        }
+
         return mediator
+    }
+
+    override fun saveContact(contact: Contact): LiveData<Resource<Void?>> {
+        saveInDatabase(contact)
+        val liveData = MutableLiveData<Resource<Void?>>()
+        liveData.value = Resource(null)
+        return liveData
+    }
+
+    private fun saveInDatabase(contact: Contact) {
+        BaseAsyncTask(
+            whenExecutes = {
+                contactDao.save(contact)
+            }, whenEnds = {}
+        ).execute()
     }
 
 }
