7784fe6e8dc6877cfe64d5d9e6466b738ac70a7d
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-02 01:48:23 -0300

    Exibindo todos os dados do contato

--- app/src/main/java/br/com/rodrigoamora/examplekoin/ui/recyclerview/viewholder/ContactViewHolder.kt
+++ app/src/main/java/br/com/rodrigoamora/examplekoin/ui/recyclerview/viewholder/ContactViewHolder.kt
@@ -6,17 +6,26 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.rodrigoamora.examplekoin.R
 import br.com.rodrigoamora.examplekoin.model.Contact
+import br.com.rodrigoamora.examplekoin.util.MaskUtil
 
 class ContactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+    lateinit var tvCellphoneContact: TextView
+    lateinit var tvEmailContact: TextView
     lateinit var tvNameContact: TextView
     lateinit var deleteContact: ImageView
 
     fun setValues(contact: Contact) {
-        tvNameContact = itemView.findViewById<TextView>(R.id.tv_name_salon)
+        tvCellphoneContact = itemView.findViewById<TextView>(R.id.tv_cellphone_contact)
+        tvCellphoneContact.text = MaskUtil.addMask(contact.cellphone, MaskUtil.FORMAT_PHONE)
+
+        tvEmailContact = itemView.findViewById<TextView>(R.id.tv_email_contact)
+        tvEmailContact.text = contact.email
+
+        tvNameContact = itemView.findViewById<TextView>(R.id.tv_name_contact)
         tvNameContact.text = contact.name
 
-        deleteContact = itemView.findViewById<ImageView>(R.id.delete_salon)
+        deleteContact = itemView.findViewById<ImageView>(R.id.delete_contact)
     }
 
 }
